434460a9d33fe5fc383e8b70fe4fec1212699c73
Théo Balick <balickmethens@gmail.com>
2022-12-26 11:11:12 +0100

    update project

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 style="color: #c8052d; font-weight: 700;"> Merry Christmas & Happy new year</h1>
+
+### Wish for christmas and Happy new year designed with HTML, CSS and vanilla JavaScript only.
+
+### 🥳🎅✨🎉🎁🎀🧧🎇🎆🎈
+<br>
+
+<h3 style="color: #e4b265;">Any improvement of your share is welcome.</h3>
+<br>
+<p style="font-weight: bolder; font-size: 1rem">
+<span style="color:red; font-weight: bolder;">‼️‼️‼️‼️</span>
+The following is optional
+<p>
+
+```bash
+# install dependencies
+$ npm install
+# serve with hot reload
+$ npm run start
+# build for production
+$ npm run build
+```
--- src/app.js
+++ src/app.js
@@ -1,30 +1,30 @@
 // create a new div element
-const mainContainer = document.createElement('div');
+const mainContainer = document.createElement("div");
 // number of div elements to add in the main container
 const divElementsNb = 150;
 let divElementsCounter = 0;
 
 // attributes id for the main container
-mainContainer.id = 'snow-container';
+mainContainer.id = "snow-container";
 
-let timerID = setInterval(function() {
-    let container = document.createElement('div');
-    const spanElements = generateSpanElements();
+let timerID = setInterval(function () {
+  let container = document.createElement("div");
+  const spanElements = generateSpanElements();
 
-    // add class name to the container
-    container.className = 'container';
+  // add class name to the container
+  container.className = "container";
 
-    // add span elements to the container
-    addSpanElements(spanElements, container);
-    // set the css styles for the span elements
-    spanElements.map(spanElement => setCssStyles(spanElement));
-    // add container div element to the main container
-    mainContainer.append(container);
-    // increment the divElements counter
-    divElementsCounter++;
-    // stop the interval when the divElementsCounter is equal to the divElementsNb
-    divElementsCounter === divElementsNb && clearInterval(timerID);
-},100)
+  // add span elements to the container
+  addSpanElements(spanElements, container);
+  // set the css styles for the span elements
+  spanElements.map((spanElement) => setCssStyles(spanElement));
+  // add container div element to the main container
+  mainContainer.append(container);
+  // increment the divElements counter
+  divElementsCounter++;
+  // stop the interval when the divElementsCounter is equal to the divElementsNb
+  divElementsCounter === divElementsNb && clearInterval(timerID);
+}, 100);
 
 // add the main container to the body
 document.body.appendChild(mainContainer);
@@ -33,16 +33,16 @@ document.body.appendChild(mainContainer);
  * @param {HTMLSpanElement} element
  */
 function setCssStyles(element) {
-    // generate random values for marginTop and marginLeft
-    const marginTopValue = Math.floor(Math.random() * 10);
-    const marginLeftValue = Math.floor(Math.random() * 10);
+  // generate random values for marginTop and marginLeft
+  const marginTopValue = Math.floor(Math.random() * 10);
+  const marginLeftValue = Math.floor(Math.random() * 10);
 
-    // set the animationName, marginTop and marginLeft values
-    element.style.marginTop = `-${marginTopValue}rem`;
-    element.style.marginLeft = `${marginLeftValue}rem`;
+  // set the animationName, marginTop and marginLeft values
+  element.style.marginTop = `-${marginTopValue}rem`;
+  element.style.marginLeft = `${marginLeftValue}rem`;
 
-    element.className += ' span';
-    //element.style.animationName = 'DownAnimation'
+  element.className += " span";
+  //element.style.animationName = 'DownAnimation'
 }
 
 /**
@@ -51,27 +51,27 @@ function setCssStyles(element) {
  * @returns {HTMLSpanElement[]} array of span elements
  */
 function generateSpanElements(numberOfElements = 100) {
-    const spanElements = [];
+  const spanElements = [];
 
-    // generate 100 (by default) span elements
-    for (let i = 0; i < numberOfElements; i++) {
-        const className = generateRandomClassName();
-        const spanElement = document.createElement('span');
+  // generate 100 (by default) span elements
+  for (let i = 0; i < numberOfElements; i++) {
+    const className = generateRandomClassName();
+    const spanElement = document.createElement("span");
 
-        spanElement.className = className; // add class name to the span element
-        spanElements.push(spanElement); // add span element to the array
-    }
+    spanElement.className = className; // add class name to the span element
+    spanElements.push(spanElement); // add span element to the array
+  }
 
-    return spanElements;
+  return spanElements;
 }
 
 /**
  * @description add span elements to the container
- * @param {HTMLSpanElement} spanElements 
- * @param {HTMLElement} container 
+ * @param {HTMLSpanElement} spanElements
+ * @param {HTMLElement} container
  */
 function addSpanElements(spanElements, container) {
-    spanElements.map(spanElement => container.appendChild(spanElement));
+  spanElements.map((spanElement) => container.appendChild(spanElement));
 }
 
 /**
@@ -79,9 +79,15 @@ function addSpanElements(spanElements, container) {
  * @returns {string} random class name
  */
 function generateRandomClassName() {
-    // list of classes to add to the span element
-    const classList = ['bull-redlight-small', 'bull-redlight', 'bull-white', 'bull-red-white'];
-    // generate a random class name
-    const randomClassName = classList[Math.floor(Math.random() * classList.length)];
-    return randomClassName;
+  // list of classes to add to the span element
+  const classList = [
+    "bull-redlight-small",
+    "bull-redlight",
+    "bull-white",
+    "bull-red-white",
+  ];
+  // generate a random class name
+  const randomClassName =
+    classList[Math.floor(Math.random() * classList.length)];
+  return randomClassName;
 }
